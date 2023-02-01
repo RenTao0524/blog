1098254227649269820e51946fb2406d6fef6cbe
uf203125 <Tao.Ren@partner.lingyue-digital.com>
2023-02-01 17:02:57 +0800

    dockerfile

--- docs/docker/dockerSidebar.ts
+++ docs/docker/dockerSidebar.ts
@@ -8,8 +8,34 @@ export const dockerSidebar = {
           link: "/docker/index.md"
         },
         {
-          text: 'docker使用',
-          link: "/docker/dockeruse.md"
+          text: '为什么使用docker',
+          link: 'https://www.php.cn/docker/486801.html'
+        },
+        {
+          text: 'docker命令',
+          collapsible: true,
+          children: [
+            {
+              text: 'run',
+              link: '/docker/dockercommand.md'
+            },
+            {
+              text: 'ps',
+              link: '/docker/dockercommand.md'
+            },
+            {
+              text: 'logs',
+              link: '/docker/dockercommand.md'
+            },
+            {
+              text: 'stop',
+              link: '/docker/dockercommand.md'
+            }
+          ],
+        },
+        {
+          text: 'Dockerfile编写',
+          link: "/docker/dockerfile/index.md"
         },
       ],
     },
---
+++ docs/docker/dockerfile/index.md
@@ -0,0 +1,3 @@
+## Dockerfile文件编写
+
+### Dockerfile常用语法
